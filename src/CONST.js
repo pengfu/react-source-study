ac37fea36790ac604a1ab3fae4e92f8cb4bb00ea
chang_su <chang_su@hunliji.com>
2021-01-08 20:35:16 +0800

    debug react源码

---
+++ src/CONST.js
@@ -0,0 +1,14 @@
+// import * as React from "react";
+// import {useState, Component} from "react";
+// import * as ReactDOM from "react-dom";
+
+/**直接调试react */
+import * as React from "./react/packages/react";
+import {useState, Component} from "./react/packages/react";
+import * as ReactDOM from "./react/packages/react-dom";
+
+// import React from "./kreact/";
+// import ReactDOM, {useState} from "./kreact/react-dom";
+// import Component from "./kreact/Component";
+
+export {React, ReactDOM, useState, Component};
